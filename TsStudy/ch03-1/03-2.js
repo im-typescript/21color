b1944b68c05cd1105c15c4056fdd51a7fdce1364
hanalee <lehhha0110@gmail.com>
2022-07-05 23:42:31 +0900

    study ~ typescript

---
+++ TsStudy/ch03-1/03-2.js
@@ -0,0 +1,9 @@
+var Person4 = /** @class */ (function () {
+    function Person4(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    return Person4;
+}());
+var jack4 = new Person4('jack', 32);
+console.log(jack4);
